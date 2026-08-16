faa792e087a660e2567e3234b910ecb95f394f39
Owner <ghifariezraramadhan@gmail.com>
2026-08-17 00:02:54 +0700

    test(optional-06): add unit tests for positive numbers summation scenario

--- Belajar Dasar Pemrograman JavaScript/optional-06-full-coverage-testing/index.test.js
+++ Belajar Dasar Pemrograman JavaScript/optional-06-full-coverage-testing/index.test.js
@@ -14,4 +14,9 @@ describe('sum function', () => {
         assert.strictEqual(sum(1, -2), 0);
         assert.strictEqual(sum(-1, -2), 0);
     });
+
+    test('should return the sum of two positive numbers', () => {
+        assert.strictEqual(sum(1, 2), 3);
+        assert.strictEqual(sum(5, 10), 15);
+    });
 });
